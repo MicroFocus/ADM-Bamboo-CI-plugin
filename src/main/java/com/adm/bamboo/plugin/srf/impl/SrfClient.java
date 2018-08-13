426dccd9ce3e082d524131fcf12c91d6cb336c56
Idan Vaytzman <vaytzman@hpe.com>
2018-08-13 15:46:26 +0300

    Handle multiple errors Normalize srf address

--- src/main/java/com/adm/bamboo/plugin/srf/impl/SrfClient.java
+++ src/main/java/com/adm/bamboo/plugin/srf/impl/SrfClient.java
@@ -67,6 +67,11 @@ public class SrfClient {
         this.sslSocketFactory = sslSocketFactory;
         this.buildLogger = buildLogger;
 
+        // Normalize SRF server URL string if needed
+        if (this.srfServerAddress.substring(this.srfServerAddress.length() - 1).equals("/")) {
+            this.srfServerAddress = this.srfServerAddress.substring(0, this.srfServerAddress.length() - 1);
+        }
+
         if (proxy != null) {
             String proxyHost = proxy.getHost();
             String proxyPort = String.format("%d",proxy.getPort());
